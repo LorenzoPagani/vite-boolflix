bc48784d657a62b3c4957d2a945e9bc7a3d0d819
Lorenzo Pagani <larrypg2@gmail.com>
2023-12-05 10:27:57 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script >
+import { store } from './store.js'
+import appHeader from './components/appHeader.vue'
+export default {
+    components: {
+        appHeader
+    },
+    data() {
+        return {
+            store,
+        }
+    }
+}
+</script>
+
+<template>
+    <appHeader></appHeader>
+</template>
+
+<style scoped></style>
---
+++ src/components/appHeader.vue
@@ -0,0 +1,27 @@
+<script>
+import { store } from "../store.js";
+export default {
+    store
+}
+</script>
+<template>
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 d-flex justify-content-between p-3">
+                    <h1>Boolflix</h1>
+                    <input v-model="store.searchInput" type="text" placeholder="cerca">
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+<style scoped>
+header {
+    background-color: black;
+}
+
+h1 {
+    color: red;
+}
+</style>
